a67e2553ffcf3aedcad7026c3301f1baa3ba4943
nezouse <stankiewicz1998@gmail.com>
2023-08-08 18:43:46 +0200

    Generate EAS contracts hooks

---
+++ contracts/Attest.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {EAS} from "eas-contracts/EAS.sol";
+
+contract Attest {
+    EAS public eas;
+}
--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from "@wagmi/cli";
-import { etherscan, react } from "@wagmi/cli/plugins";
+import { foundry, react } from "@wagmi/cli/plugins";
 
 export default defineConfig({
   out: "src/generated.ts",
   contracts: [],
-  plugins: [
-    etherscan({
-      apiKey: "RPKYAHCE6R2YI7TRV51WS5N8R885RRNXG3",
-      chainId: 420,
-      contracts: [
-        {
-          name: "AttestationStation",
-          address: "0x4200000000000000000000000000000000000021",
-        },
-      ],
-    }),
-    react(),
-  ],
+  plugins: [foundry(), react()],
 });
