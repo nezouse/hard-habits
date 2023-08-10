8d28bdb89ade60049de633c300005ad1530da191
nezouse <stankiewicz1998@gmail.com>
2023-08-10 14:31:01 +0200

    Add attestation to redeem and task failed

--- contracts/PublicPool.sol
+++ contracts/PublicPool.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {IEAS, AttestationRequest, AttestationRequestData} from "eas-contracts/IEAS.sol";
+import {IEAS, AttestationRequest, AttestationRequestData, Attestation} from "eas-contracts/IEAS.sol";
 import {NO_EXPIRATION_TIME, EMPTY_UID} from "eas-contracts/Common.sol";
 
 contract PublicPool is ERC20 {
@@ -15,11 +15,33 @@ contract PublicPool is ERC20 {
 
     IEAS private immutable _eas;
 
-    bytes32 public constant DEPOSIT_SCHEMA_ID = keccak256(
-        abi.encodePacked("string category,uint256 stake,uinty256 shares,uint64 endDate,uint64 value", address(0), false)
-    );
+    bytes32 public constant DEPOSIT_SCHEMA_ID =
+        keccak256(
+            abi.encodePacked(
+                "string category,uint256 stake,uinty256 shares,uint64 endDate,uint64 value",
+                address(0),
+                false
+            )
+        );
 
-    constructor(IERC20 underlyingAsset, IEAS eas) ERC20("PublicHabitPool", "PHP") {
+    bytes32 public constant REDEEM_SCHEMA_ID =
+        keccak256(
+            abi.encodePacked("string type, string proofUrl", address(0), false)
+        );
+
+    bytes32 public constant TASK_FAILED_SCHEMA_ID =
+        keccak256(abi.encodePacked("string type", address(0), false));
+
+    struct DepositData {
+        string category;
+        uint64 endDate;
+        uint64 value;
+    }
+
+    constructor(
+        IERC20 underlyingAsset,
+        IEAS eas
+    ) ERC20("PublicHabitPool", "PHP") {
         _asset = underlyingAsset;
         _underlyingDecimals = ERC20(address(underlyingAsset)).decimals();
         _eas = eas;
@@ -37,33 +59,105 @@ contract PublicPool is ERC20 {
         return _asset.balanceOf(address(this));
     }
 
-    function deposit(uint256 assets) external returns (bytes32) {
+    function deposit(
+        uint256 assets,
+        DepositData calldata depositData
+    ) external returns (bytes32) {
         uint256 shares = previewDeposit(assets);
         _asset.transferFrom(msg.sender, address(this), assets);
         _mint(msg.sender, shares);
-        return _eas.attest(
-            AttestationRequest({
-                schema: DEPOSIT_SCHEMA_ID,
-                data: AttestationRequestData({
-                    recipient: msg.sender,
-                    expirationTime: NO_EXPIRATION_TIME,
-                    revocable: false,
-                    refUID: EMPTY_UID,
-                    value: 0,
-                    data: abi.encode("Steps", assets, shares, 0, 0)
+        return
+            _eas.attest(
+                AttestationRequest({
+                    schema: DEPOSIT_SCHEMA_ID,
+                    data: AttestationRequestData({
+                        recipient: msg.sender,
+                        expirationTime: NO_EXPIRATION_TIME,
+                        revocable: false,
+                        refUID: EMPTY_UID,
+                        value: 0,
+                        data: abi.encode(
+                            depositData.category,
+                            assets,
+                            shares,
+                            depositData.endDate,
+                            depositData.value
+                        )
+                    })
                 })
-            })
-        );
+            );
     }
 
-    function redeem(uint256 shares) external {
+    function redeem(
+        bytes32 attestationId,
+        string calldata proofUrl
+    ) external returns (bytes32) {
+        Attestation memory attestation = _eas.getAttestation(attestationId);
+
+        require(
+            attestation.schema == DEPOSIT_SCHEMA_ID,
+            "PP: invalid attestation"
+        );
+
+        (, , uint256 shares, uint64 endDate, ) = abi.decode(
+            attestation.data,
+            (string, uint256, uint256, uint64, uint64)
+        );
+
+        require(block.timestamp <= endDate, "PP: only before endDate");
+        require(attestation.recipient == msg.sender, "PP: only recipient");
+
         uint256 assets = previewRedeem(shares);
         _burn(msg.sender, shares);
         _asset.transfer(msg.sender, assets);
+
+        return
+            _eas.attest(
+                AttestationRequest({
+                    schema: REDEEM_SCHEMA_ID,
+                    data: AttestationRequestData({
+                        recipient: msg.sender,
+                        expirationTime: NO_EXPIRATION_TIME,
+                        revocable: false,
+                        refUID: attestationId,
+                        value: 0,
+                        data: abi.encode("Redeem", proofUrl)
+                    })
+                })
+            );
     }
 
-    function taskFailed(uint256 amount) external {
-        _burn(msg.sender, amount);
+    function taskFailed(bytes32 attestationId) external returns (bytes32) {
+        Attestation memory attestation = _eas.getAttestation(attestationId);
+
+        require(
+            attestation.schema == DEPOSIT_SCHEMA_ID,
+            "PP: invalid attestation"
+        );
+
+        (, , uint256 shares, uint64 endDate, ) = abi.decode(
+            attestation.data,
+            (string, uint256, uint256, uint64, uint64)
+        );
+
+        require(block.timestamp > endDate, "PP: only after endDate");
+
+        _burn(attestation.recipient, shares);
+
+        return
+            _eas.attest(
+                AttestationRequest({
+                    schema: TASK_FAILED_SCHEMA_ID,
+                    data: AttestationRequestData({
+                        recipient: attestation.recipient,
+                        expirationTime: NO_EXPIRATION_TIME,
+                        revocable: false,
+                        refUID: attestationId,
+                        value: 0,
+                        data: abi.encode("TaskFailed")
+                    })
+                })
+            );
     }
 
     function previewRedeem(uint256 shares) public view returns (uint256) {
--- contracts/test/PublicPool.t.sol
+++ contracts/test/PublicPool.t.sol
@@ -15,15 +15,23 @@ contract PublicPoolTest is Test {
     USDC public usdc;
     SchemaRegistry public registry;
     EAS public eas;
-    bytes32 public schemaId;
+
+    PublicPool.DepositData public defaultDepositData =
+        PublicPool.DepositData("test", 1, 0);
 
     function setUp() public {
         registry = new SchemaRegistry();
-        schemaId = registry.register(
+        registry.register(
             "string category,uint256 stake,uinty256 shares,uint64 endDate,uint64 value",
             ISchemaResolver(address(0)),
             false
         );
+        registry.register(
+            "string type, string proofUrl",
+            ISchemaResolver(address(0)),
+            false
+        );
+        registry.register("string type", ISchemaResolver(address(0)), false);
 
         eas = new EAS(registry);
 
@@ -44,7 +52,7 @@ contract PublicPoolTest is Test {
         usdc.mint(address(this), 1000000);
         usdc.approve(address(pool), 1000000);
 
-        pool.deposit(1000000);
+        pool.deposit(1000000, defaultDepositData);
 
         assertEq(pool.totalSupply(), 1000000);
         assertEq(pool.totalAssets(), 1000000);
@@ -55,8 +63,8 @@ contract PublicPoolTest is Test {
     function testRedeem() public {
         usdc.mint(address(this), 1000000);
         usdc.approve(address(pool), 1000000);
-        pool.deposit(1000000);
-        pool.redeem(1000000);
+        bytes32 depositId = pool.deposit(1000000, defaultDepositData);
+        pool.redeem(depositId, "");
 
         assertEq(pool.totalSupply(), 0);
         assertEq(pool.totalAssets(), 0);
@@ -67,8 +75,9 @@ contract PublicPoolTest is Test {
     function testTaskFailed() public {
         usdc.mint(address(this), 1000000);
         usdc.approve(address(pool), 1000000);
-        pool.deposit(1000000);
-        pool.taskFailed(1000000);
+        bytes32 depositId = pool.deposit(1000000, defaultDepositData);
+        vm.warp(block.timestamp + 1);
+        pool.taskFailed(depositId);
 
         assertEq(pool.totalSupply(), 0);
         assertEq(pool.totalAssets(), 1000000);
@@ -76,21 +85,28 @@ contract PublicPoolTest is Test {
     }
 
     function testRedeemAfterFailed() public {
+        PublicPool.DepositData memory depositData = PublicPool.DepositData(
+            "Steps",
+            uint64(block.timestamp + 10),
+            64
+        );
+
         uint256 amount = 1000000;
         usdc.mint(address(this), amount);
         usdc.approve(address(pool), amount);
-        pool.deposit(amount);
+        bytes32 depositId = pool.deposit(amount, depositData);
 
         address prankAddress = 0x0000000000000000000000000000000000000001;
         usdc.mint(prankAddress, amount);
 
         vm.startPrank(prankAddress);
         usdc.approve(address(pool), amount);
-        pool.deposit(amount);
-        pool.taskFailed(amount);
+        bytes32 deposit2Id = pool.deposit(amount, defaultDepositData);
+        vm.warp(block.timestamp + 1);
+        pool.taskFailed(deposit2Id);
         vm.stopPrank();
 
-        pool.redeem(amount);
+        pool.redeem(depositId, "");
 
         assertEq(pool.totalSupply(), 0);
         assertEq(pool.totalAssets(), 0);
@@ -101,20 +117,21 @@ contract PublicPoolTest is Test {
         uint256 amount = 1000000;
         usdc.mint(address(this), amount);
         usdc.approve(address(pool), amount);
-        pool.deposit(amount);
+        pool.deposit(amount, defaultDepositData);
 
         address prankAddress = 0x0000000000000000000000000000000000000001;
         usdc.mint(prankAddress, amount);
 
         vm.startPrank(prankAddress);
         usdc.approve(address(pool), amount);
-        pool.deposit(amount);
-        pool.taskFailed(amount);
+        bytes32 depositId = pool.deposit(amount, defaultDepositData);
+        vm.warp(block.timestamp + 1);
+        pool.taskFailed(depositId);
         vm.stopPrank();
 
         usdc.mint(address(this), amount);
         usdc.approve(address(pool), amount);
-        pool.deposit(amount);
+        pool.deposit(amount, defaultDepositData);
 
         assertEq(pool.totalSupply(), (amount * 3) / 2);
         assertEq(pool.balanceOf(address(this)), (amount * 3) / 2);
@@ -122,22 +139,114 @@ contract PublicPoolTest is Test {
     }
 
     function testDepositCreatesAttestation() public {
+        PublicPool.DepositData memory depositData = PublicPool.DepositData(
+            "Steps",
+            uint64(block.timestamp + 10),
+            64
+        );
+
         usdc.mint(address(this), 1000000);
         usdc.approve(address(pool), 1000000);
-        bytes32 id = pool.deposit(1000000);
+        bytes32 id = pool.deposit(1000000, depositData);
 
         Attestation memory attestation = eas.getAttestation(id);
-        assertEq(attestation.schema, schemaId);
+        assertEq(attestation.schema, pool.DEPOSIT_SCHEMA_ID());
         assertEq(attestation.recipient, address(this));
         assertEq(attestation.revocable, false);
 
-        (string memory category, uint256 stake, uint256 shares, uint64 endDate, uint64 value) =
-            abi.decode(attestation.data, (string, uint256, uint256, uint64, uint64));
+        (
+            string memory category,
+            uint256 stake,
+            uint256 shares,
+            uint64 endDate,
+            uint64 value
+        ) = abi.decode(
+                attestation.data,
+                (string, uint256, uint256, uint64, uint64)
+            );
 
-        assertEq(category, "Steps");
+        assertEq(category, depositData.category);
         assertEq(stake, 1000000);
         assertEq(shares, 1000000);
-        assertEq(endDate, 0);
-        assertEq(value, 0);
+        assertEq(endDate, depositData.endDate);
+        assertEq(value, depositData.value);
+    }
+
+    function testReedemRevertsWithWrongSchema() public {
+        vm.expectRevert(bytes("PP: invalid attestation"));
+        pool.redeem(bytes32(uint256(1)), "");
+    }
+
+    function testReedemRevertsAfterEndDate() public {
+        usdc.mint(address(this), 1000000);
+        usdc.approve(address(pool), 1000000);
+        bytes32 id = pool.deposit(1000000, defaultDepositData);
+
+        vm.warp(block.timestamp + 1000);
+
+        vm.expectRevert(bytes("PP: only before endDate"));
+        pool.redeem(id, "");
+    }
+
+    function testReedemRevertsIfNotRecipient() public {
+        usdc.mint(address(this), 1000000);
+        usdc.approve(address(pool), 1000000);
+        bytes32 id = pool.deposit(1000000, defaultDepositData);
+
+        vm.expectRevert(bytes("PP: only recipient"));
+        vm.prank(address(1));
+        pool.redeem(id, "");
+    }
+
+    function testTaskFailedBurnsSharesFromRecipient() public {
+        usdc.mint(address(this), 1000000);
+        usdc.approve(address(pool), 1000000);
+        bytes32 depositId = pool.deposit(1000000, defaultDepositData);
+        vm.warp(block.timestamp + 1);
+        vm.prank(address(1));
+        pool.taskFailed(depositId);
+
+        assertEq(pool.totalSupply(), 0);
+        assertEq(pool.totalAssets(), 1000000);
+        assertEq(pool.balanceOf(address(this)), 0);
+    }
+
+    function testRedeemCreatesAttestation() public {
+        usdc.mint(address(this), 1000000);
+        usdc.approve(address(pool), 1000000);
+        bytes32 depositId = pool.deposit(1000000, defaultDepositData);
+
+        bytes32 redeemId = pool.redeem(depositId, "url");
+
+        Attestation memory attestation = eas.getAttestation(redeemId);
+        assertEq(attestation.schema, pool.REDEEM_SCHEMA_ID());
+        assertEq(attestation.recipient, address(this));
+        assertEq(attestation.revocable, false);
+
+        (string memory category, string memory proofUrl) = abi.decode(
+            attestation.data,
+            (string, string)
+        );
+
+        assertEq(category, "Redeem");
+        assertEq(proofUrl, "url");
+    }
+
+    function testTaskFailedCreatesAttestation() public {
+        usdc.mint(address(this), 1000000);
+        usdc.approve(address(pool), 1000000);
+        bytes32 depositId = pool.deposit(1000000, defaultDepositData);
+
+        vm.warp(block.timestamp + 1);
+        bytes32 failedId = pool.taskFailed(depositId);
+
+        Attestation memory attestation = eas.getAttestation(failedId);
+        assertEq(attestation.schema, pool.TASK_FAILED_SCHEMA_ID());
+        assertEq(attestation.recipient, address(this));
+        assertEq(attestation.revocable, false);
+
+        string memory category = abi.decode(attestation.data, (string));
+
+        assertEq(category, "TaskFailed");
     }
 }
