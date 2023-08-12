20bbebb2a36c293ef928600e235d53baa1fc0bd0
nezouse <stankiewicz1998@gmail.com>
2023-08-12 21:46:27 +0200

    Update providers

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,18 +1,14 @@
 import { configureChains, createConfig } from "wagmi";
-import { optimism, optimismGoerli } from "wagmi/chains";
+import { optimismGoerli } from "wagmi/chains";
 
 import { publicProvider } from "wagmi/providers/public";
-import { infuraProvider } from "wagmi/providers/infura";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 const walletConnectProjectId = "cec4e73ceb25170b43bd2dbf802a7f95";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [optimism, optimismGoerli],
-  [
-    infuraProvider({ apiKey: "b8ed6f629b514a4fa5d9503c5798451f" }),
-    publicProvider(),
-  ]
+  [optimismGoerli],
+  [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
