898352995137db134f4e946ea2221aec13ce690c
nezouse <stankiewicz1998@gmail.com>
2023-08-13 13:45:23 +0200

    Update feed header

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -6,9 +6,10 @@ export const dynamic = "force-dynamic";
 export default async function Page() {
   const attestations = await getAllAttestations();
   return (
-    <div className="mx-auto max-w-5xl">
-      <div className="mb-4">All posts</div>
-      <div className="flex flex-wrap justify-center gap-4">
+    <div className="mx-auto max-w-5xl p-9">
+      <div className="mb-4 text-3xl">All posts</div>
+      <div>Don't stalk people too hard</div>
+      <div className="flex flex-wrap justify-center gap-4 mt-6">
         {attestations.map((attestation) => (
           <HabitCard key={attestation.id} attestation={attestation} />
         ))}
