5c97b455046db2f24d45606358b1365b0509eb42
nezouse <stankiewicz1998@gmail.com>
2023-08-13 15:03:18 +0200

    Deploy on mode network

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -7,7 +7,32 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 const walletConnectProjectId = "cec4e73ceb25170b43bd2dbf802a7f95";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [optimismGoerli, baseGoerli, zoraTestnet],
+  [
+    optimismGoerli,
+    baseGoerli,
+    zoraTestnet,
+    {
+      id: 919,
+      name: "Mode Testnet",
+      network: "mode-testnet",
+      nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+      rpcUrls: {
+        default: {
+          http: ["https://sepolia.mode.network"],
+        },
+        public: {
+          http: ["https://sepolia.mode.network"],
+        },
+      },
+      blockExplorers: {
+        default: {
+          name: "Blockscout",
+          url: "https://sepolia.explorer.mode.network",
+        },
+      },
+      testnet: true,
+    },
+  ],
   [publicProvider()]
 );
 
