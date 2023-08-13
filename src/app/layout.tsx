53f41bc9b63b1a95091dd23cfb0a03357845146a
nezouse <stankiewicz1998@gmail.com>
2023-08-13 13:07:21 +0200

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@uploadthing/react/styles.css";
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import { GithubIcon } from "lucide-react";
 
 import { Providers } from "./providers";
 import Link from "next/link";
@@ -19,10 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="p-4">
+      <body className="min-h-screen">
         <Providers>
           <Nav />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
@@ -31,7 +33,7 @@ export default function RootLayout({
 
 function Nav() {
   return (
-    <nav className="flex justify-between mb-12 ">
+    <nav className="flex justify-between mb-12 mx-6 mt-5">
       <div>
         <Button variant="link" asChild className="text-2xl text-yellow-950">
           <Link href="/">💪 Hard habits</Link>
@@ -46,3 +48,44 @@ function Nav() {
     </nav>
   );
 }
+
+export const Footer = () => {
+  return (
+    <div className="relative mt-48 bg-blue-200 text-blue-950">
+      <svg
+        className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-blue-200"
+        preserveAspectRatio="none"
+        viewBox="0 0 1440 54"
+      >
+        <path
+          fill="currentColor"
+          d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
+        />
+      </svg>
+      <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+          <div className="md:max-w-md lg:col-span-2">
+            <div className="mt-4 lg:max-w-sm">
+              <p className="text-sm">
+                Lets try to get the best of us. Keep track of your habits and be
+                the best version of yourself.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+          <p className="text-sm ">2023, Hard habits. All the fun for you.</p>
+          <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+            <a
+              href="https://github.com/nezouse/hard-habits"
+              target="_blank"
+              className="transition-colors duration-300 hover:text-blue-400"
+            >
+              <GithubIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,50 +15,52 @@ import PublicPool from "@/images/publicPool.png";
 
 export default function Page() {
   return (
-    <div className="grid-cols-1 grid lg:grid-cols-2 gap-8 max-w-5xl mx-auto">
-      <Card>
-        <CardHeader>
-          <CardTitle> Public pool</CardTitle>
-          <CardDescription>
-            Share your achievements with everyone
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Image
-            src={PublicPool}
-            height={320}
-            alt="A group of people sitting together."
-            className="mx-auto"
-          />
-        </CardContent>
-        <CardFooter>
-          <Button asChild className="mx-auto w-40">
-            <Link href="/publicPool/create">Join pool</Link>
-          </Button>
-        </CardFooter>
-      </Card>
+    <div>
+      <div className="grid-cols-1 grid lg:grid-cols-2 gap-8 max-w-5xl mx-auto">
+        <Card>
+          <CardHeader>
+            <CardTitle> Public pool</CardTitle>
+            <CardDescription>
+              Share your achievements with everyone
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Image
+              src={PublicPool}
+              height={320}
+              alt="A group of people sitting together."
+              className="mx-auto"
+            />
+          </CardContent>
+          <CardFooter>
+            <Button asChild className="mx-auto w-40">
+              <Link href="/publicPool/create">Join pool</Link>
+            </Button>
+          </CardFooter>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Private pool</CardTitle>
-          <CardDescription>
-            Compete with your friends and family
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Image
-            src={PrivatePool}
-            height={320}
-            alt="A girl sitting alone."
-            className="mx-auto"
-          />
-        </CardContent>
-        <CardFooter>
-          <Button className="mx-auto w-40" disabled>
-            Create pool
-          </Button>
-        </CardFooter>
-      </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Private pool</CardTitle>
+            <CardDescription>
+              Compete with your friends and family
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Image
+              src={PrivatePool}
+              height={320}
+              alt="A girl sitting alone."
+              className="mx-auto"
+            />
+          </CardContent>
+          <CardFooter>
+            <Button className="mx-auto w-40" disabled>
+              Create pool
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
